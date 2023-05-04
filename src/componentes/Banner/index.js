694aa59c9e6b55a94914b79a748490db08f4a7a4
Kalebe <kalebemartins220@gmail.com>
2023-05-03 21:03:01 -0300

    Initial commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+function Banner() {
+   return (
+      <header>
+         <img src="./imagens/banner.png" className="banner" alt="banner da página"/>
+      </header>
+   )
+}
+
+export default Banner
